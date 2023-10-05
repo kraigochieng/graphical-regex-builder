3f19b6426b02db2f3747c22fa2dba28cd880823c
kraigochieng <kraigochieng@gmail.com>
2023-10-05 16:03:05 +0300

    Removed react.strictmode because it was intefering with react-beautiful-dnd

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,9 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
+// Removed React.StrictMode for react-beautiful-dnd
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <App />,
+
 )
